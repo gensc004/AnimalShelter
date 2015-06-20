fbb989420c386004e9fded393e5c5a18fae733be
gensc004 <gensc004@morris.umn.edu>
2015-06-20 14:34:22 -0500

    Routes!

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -7,6 +7,19 @@ angular.module('animalShelterApp')
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
     });
-    
+    $scope.isOpen = false;
+      $scope.demo = {
+        isOpen: false,
+        count: 0,
+        selectedAlignment: 'md-left'
+      };
+     var iconData = [
+            {name: 'icon-home'        , color: "#777" },
+            {name: 'icon-user-plus'   , color: "rgb(89, 226, 168)" },
+            {name: 'icon-google-plus2', color: "#A00" },
+            {name: 'icon-youtube4'    , color:"#00A" },
+             // Use theming to color the font-icon
+            {name: 'icon-settings'    , color:"#A00", theme:"md-warn md-hue-5"}
+          ];
 
   });
